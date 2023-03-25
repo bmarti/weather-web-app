b16f1464d642ba0681b904e6b360e27859ee5c64
Baptiste <31932714+bmarti@users.noreply.github.com>
2023-03-25 20:42:09 +0100

    Implement search bar to find cities

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd163eb4938msh5c265d79bff9883p154023jsne81d905096c2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
